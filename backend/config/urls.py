fd1b6a6b5777f17428c52bb4a6263e463d6c68ae
Roberton003 <roberton003@users.noreply.github.com>
2025-10-22 20:40:23 -0300

    fix: Adicionar rota raiz que serve index.html do React

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -38,4 +39,7 @@ urlpatterns = [
     # API Documentation
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    
+    # React SPA root
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
 ]
